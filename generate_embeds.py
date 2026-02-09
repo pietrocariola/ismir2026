c7fa14422cd91a229a83f735b05c75b3eb2954fe
Pietro <pietrocariola@gmail.com>
2026-02-09 17:58:44 +0000

    try except added

--- generate_embeds.py
+++ generate_embeds.py
@@ -5,6 +5,9 @@ import librosa
 import laion_clap
 from pathlib import Path
 import numpy as np
+import pandas as pd
+import uuid
+from tqdm import tqdm
 
 CLAP_SR = 48000 # paper requires clap with 48kHz sample rate
 CLAP_FUSION = False # fusion or not snipets from the first middle and last third of the sample
@@ -25,44 +28,88 @@ def main():
     ds_path = args.dataset_path
     files = os.listdir(ds_path)
     transformations = args.transformations
-    ds_name = args.dataset_name
+    ds_name = args.dataset_name.lower().replace("_", "")
     out_path = args.output_path
 
     os.makedirs("embeds", exist_ok=True)
+    if Path("metadata.csv").exists():
+        df = pd.read_csv("metadata.csv")
+    else:
+        df = pd.DataFrame({
+            "ds_name": [],
+            "file": [],
+            "file_path": [],
+            "model": [],
+            "transf": [],
+            "transf_param_name": [],
+            "file_embeds": [],
+            "file_embeds_path": []
+        })
 
-    for m in models:
+    n_tfs = 0
+    for t in transformations:
+        n_tfs += max(1, len(tf.tf_dict_params[t]))
+
+    for model in models:
+        model = model.lower().replace("_", "")
+
         ### CLAP ###
-        if m.lower() == "clap":
-            model = laion_clap.CLAP_Module(enable_fusion=CLAP_FUSION)
-            model.load_ckpt() # download the default pretrained checkpoint.
-            for t in transformations:
-                tparams = tf.tf_dict_params[t] # params set in transformation.py
-                for tparam in tparams:
-                    for f in files[:5]:
-                        m = m.lower().replace("_", "")
-                        ds_name = ds_name.lower().replace("_", "")
-                        t = t.lower().replace("_", "")
-                        tparam_name = str(tparam).lower().replace("_", "")
-                        f = f.lower().replace("_", "")
-                        p = os.path.join(out_path, 
-                            f"{m}_{ds_name}_{t}_{tparam_name}_{str(f).split(".")[0]}.npy")
-                        path = Path(p)
-                        if not path.exists():
-                            y, sr = librosa.load(os.path.join(ds_path, f), sr=CLAP_SR)
-                            y, sr = tf.tf_dict[t](y, sr, tparam)
-                            y = y.reshape(1, -1)
-                            y = model.get_audio_embedding_from_data(
-                                x = y, use_tensor=False)
-                            np.save(p, y)
-            del(model)
-                    
-
-                    
-
-
-            
-
-       
+        if model == "clap":
+            mdl = laion_clap.CLAP_Module(enable_fusion=CLAP_FUSION)
+            mdl.load_ckpt() # download the default pretrained checkpoint.
+            for file in tqdm(files[:2]):
+                df_new = pd.DataFrame({
+                        "ds_name": [],
+                        "file": [],
+                        "file_path": [],
+                        "model": [],
+                        "transf": [],
+                        "transf_param_name": [],
+                        "file_embeds": [],
+                        "file_embeds_path": []
+                    })
+                try:
+                    y, sr = librosa.load(os.path.join(ds_path, file), sr=CLAP_SR)
+                    for transf in transformations:
+                        transf = transf.lower().replace("_", "")
+                        tfparams = tf.tf_dict_params[transf] # params set in transformation.py
+                        for tfparam in tfparams:
+                            transf_param_name = str(tfparam).lower().replace("_", "")[:5]                                      
+                            file_embeds = f"{ds_name}_{file.split(".")[0].replace("_", "").lower() \
+                                                    }_{model}_{transf}_{transf_param_name}.npy"
+                            file_write_path = os.path.join(out_path, file_embeds)
+                            if df.loc[
+                                (df["ds_name"]==ds_name) &
+                                (df["file"]==file) &
+                                (df["model"]==model) &
+                                (df["transf"]==transf) &
+                                (df["transf_param_name"]==transf_param_name) &
+                                (df["file_embeds"]==file_embeds)
+                            ].empty:                            
+                                x, sr = tf.tf_dict[transf](y, sr, tfparam)
+                                x = x.reshape(1, -1) # (n_channels, embeds_size)
+                                x = mdl.get_audio_embedding_from_data(
+                                    x = x, use_tensor=False)
+                                np.save(file_write_path, x)
+                                new_row = pd.DataFrame([{
+                                    "ds_name": ds_name,
+                                    "file": file,
+                                    "file_path": os.path.abspath(os.path.join(ds_path, file)),
+                                    "model": model,
+                                    "transf": transf,
+                                    "transf_param_name": transf_param_name,
+                                    "file_embeds": file_embeds,
+                                    "file_embeds_path": os.path.abspath(file_write_path)
+                                }])
+                                df_new = pd.concat([df_new, new_row], ignore_index=True)
+                except Exception:
+                    del(df_new)
+                    print(f"Exception in file: {file}")
+                    continue
+                df = pd.concat([df, df_new], ignore_index=True)        
+                del(df_new)                
+            del(mdl)   
+    df.to_csv("metadata.csv", index=False)   
 
 if __name__ == "__main__":
     main()
